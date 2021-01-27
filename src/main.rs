70bf5e64b40adbef01a984369f49ff7bb4e0b7ec
4ist <frichardson062@gmail.com>
2021-01-26 20:28:54 -0600

    first pass at automated commit logic implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fs;
+use std::process::Command;
+
+
 fn main() {
     println!("Hello, world!");
+    //let data = fs::read_to_string("config.json").expect("Unable to read file");
+    // println!("{}", data);
+
+    //let json: serde_json::Value = serde_json::from_str(&data);
+
+    let file = fs::File::open("config.json")
+        .expect("file should open read only");
+
+    let app_config: serde_json::Value = serde_json::from_reader(file)
+        .expect("file should be proper JSON");
+
+    let os = app_config.get("OS")
+        .expect("file should have OS key");
+
+    //println!("{}", &os);}
+    
+    let commands = vec!["git add .", "git commit -m \"automated commit \"", "git push"];
+    let iterable_commands = commands.iter();
+
+    for command in iterable_commands{
+        let output = {
+            Command::new("cmd")
+                    .args(&["/C", &command])
+                    .status()
+                    .expect("failed to execute process")
+        };
+    }
+
+    
+    
+    //let hello = output.stdout;
+
+
+
 }
+

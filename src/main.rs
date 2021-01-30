5255714dd9f79e3bb32ceb88bcff66e11508e3b0
4ist <frichardson062@gmail.com>
2021-01-30 12:23:27 -0600

    automated commit

--- src/main.rs
+++ src/main.rs
@@ -5,68 +5,74 @@ use std::io;
 use config::Config;
 use std::collections::HashMap;
 use std::path::PathBuf;
+use std::slice::Iter;
 
 fn main() {
+    println!("-----------------");
+    println!("| file-watch.rs |");
+    println!("-----------------");
+
+    let current_dir = env::current_dir().unwrap();
+    println!("Target directory: {}", current_dir.display());
+
+    let mut exe_dir = env::current_exe().unwrap();
+    exe_dir.pop();
+
+    println!("Config path: {}", exe_dir.display());
+
+    let mut config_path: String = String::from(exe_dir.to_str().expect("current exe path was null"));
+    config_path.push_str("\\config");
+
+
+    let mut config = Config::default();
+    config.merge(config::File::with_name(&config_path)).unwrap();
+    let lil_config = config.try_into::<HashMap<String, String>>().unwrap();
+    println!("Config: {:?}", lil_config);
     
-
-   // println!("configgggg - {:?}", lil_config.try_into::<HashMap<String, String>>().unwrap());
-
-    let dir = env::current_dir().unwrap();
-
-    //let adir = inner_main().expect("Couldn't");
-    let mut adir = env::current_exe().unwrap();
-    adir.pop();
-
-    println!("file-watch.rs: pushing files in {}", dir.display());
-    println!("file-watch.rs: pushing files in {}", adir.display());
-
-    /*
-    let mut lil_config = Config::default();
-    lil_config
-         .merge(adir);
-        .merge(config::File::with_name("config")).unwrap();
-        //.merge(config::Environment::with_prefix("APP")).unwrap();
-    */
-
-
-    /*
-    let file = fs::File::open("config.json")
-        .expect("file should open read only");
-
-    let app_config: serde_json::Value = serde_json::from_reader(file)
-        .expect("file should be proper JSON");
-
-    let os = app_config.get("OS")
-        .expect("file should have OS key");
-    */
-
-    //println!("{}", &os);}
-    let git_add = vec!["/C", "git", "add", "."]; //TODO add this to a try/catch to handle being ran outside a git repo
-    let git_commit = vec!["/C", "git", "commit", "-m", "automated commit without double quotes test"];
-    let git_push = vec!["/C", "git", "push"]; //TODO add this to a try/catch to handle repos without remote master
-
-    let commands = vec![&git_add, &git_commit, &git_push];
-    let iterable_commands = commands.iter();
-
-    let enabled = false;
+    let enabled = true;
     if !enabled{
         println!("committing not enabled; exiting");
         return
     }
-    for &command in iterable_commands{
-        let output = {
-            Command::new("cmd")
+
+    let os = lil_config.get("OS").expect("file should have OS key");
+    let commands = get_commands(&os);
+    let iterable_commands = commands.iter();
+
+    for command in iterable_commands{
+        let _output = {
+            Command::new("cmd") //Refactor this
                     .args(&*command)
                     .status()
                     .expect("failed to execute process")
         };
     }
-
     
-    
-    //let hello = output.stdout;
+}
 
+fn get_commands(os: &str) -> Vec<Vec<String>>{   //TODO figure out how to return an iterable
+    match &os[..] {
+        "windows" => get_windows_commands(),
+        //"linux" => println!("OS config = linux"),
+        _ => panic!("unsupported OS config: {}", &os[..])
+    }
+}
 
+macro_rules! string_vec {
+    // match a list of expressions separated by comma:
+    ($($str:expr),*) => ({
+        // create a Vec with this list of expressions,
+        // calling String::from on each:
+        vec![$(String::from($str),)*] as Vec<String>
+    });
+}
 
+fn get_windows_commands() -> Vec<Vec<String>> { 
+    let git_add = string_vec!["/C", "git", "add", "."]; //TODO add this to a try/catch to handle being ran outside a git repo
+    let git_commit = string_vec!["/C", "git", "commit", "-m", "automated commit"];
+    let git_push = string_vec!["/C", "git", "push"]; //TODO add this to a try/catch to handle repos without remote master
+
+    let commands = vec![git_add, git_commit, git_push];
+    return commands;
 }
 
